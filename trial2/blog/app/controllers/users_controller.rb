ee5d7a6be7594887b33edb6aeeb7bb189ffe2e8b
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-18 15:09:35 -0800

    Added new and create methods and views; added bcrypt

--- trial2/blog/app/controllers/users_controller.rb
+++ trial2/blog/app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+    redirect_to root_path
   end
 
   def edit
