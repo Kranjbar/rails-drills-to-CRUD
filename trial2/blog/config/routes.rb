ebf1969b3c140185a7d2ea2b39ae0c6348563d09
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-18 16:20:13 -0800

    Associated models and added routes for articles

--- trial2/blog/config/routes.rb
+++ trial2/blog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	root to: "users#index"
-  resources :users
+  resources :users do
+  	resources :articles
+  end
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
